1905bc4993a137447677b5d84d781b1b3dee0556
leogp <leogp@192.168.0.17>
2024-05-29 06:06:06 -0300

    olá mundo

---
+++ curso_programacao_udemy/src/curso_programacao/processamentodados.java
@@ -0,0 +1,23 @@
+package curso_programacao;
+
+
+public class processamentodados {
+
+	public static void main(String[] args) {
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		//o uso do double entre parênteses a seguir serve para fazer o casting, que é a conversão explícita
+		//da variável a em número flutuante.
+		resultado = (double) a / b;
+		
+		
+		System.out.println(resultado);
+		
+			
+	}
+
+}
